6eea37ec4ed09eb60c01abdcd947d151438393fa
Suissa <jnascimento@gmail.com>
2016-08-17 17:01:18 -0300

    Modularizando o crawler, correção do options

--- src/index-promise-obj.js
+++ src/index-promise-obj.js
@@ -4,70 +4,70 @@ const request = require('request')
 const rp = require('request-promise');
 const cheerio = require('cheerio')
 
-const BASE_URL = 'http://www.botanica.org.br/rbh-catalogo'
-const BASE_URL_PAGE = 'http://www.botanica.org.br/rede_herbarios.php?_pagi_pg='
+// const BASE_URL = 'http://www.botanica.org.br/rbh-catalogo'
+// const BASE_URL_PAGE = 'http://www.botanica.org.br/rede_herbarios.php?_pagi_pg='
 
-const infos = '.tx_dados_herb'
+// const infos = '.tx_dados_herb'
 
-const ElementList = '.tx_dados_herb'
-const Fields = [
-  {
-    name: '',
-    value: 'this.children[0].data'
-  },
-  {
-    name: 'Instituicao',
-    value: 'this.children[0].data'
-  },
-  {
-    name: 'Departamento',
-    value: 'this.children[0].data'
-  },
-  {
-    name: 'Endereco',
-    value: 'this.children[0].data'
-  },
-  {
-    name: 'MunicipioUF',
-    value: 'this.children[0].data'
-  }
-]
+// const ElementList = '.tx_dados_herb'
+// const Fields = [
+//   {
+//     name: '',
+//     value: 'this.children[0].data'
+//   },
+//   {
+//     name: 'Instituicao',
+//     value: 'this.children[0].data'
+//   },
+//   {
+//     name: 'Departamento',
+//     value: 'this.children[0].data'
+//   },
+//   {
+//     name: 'Endereco',
+//     value: 'this.children[0].data'
+//   },
+//   {
+//     name: 'MunicipioUF',
+//     value: 'this.children[0].data'
+//   }
+// ]
 
-const optionsRequest = {
-    uri: BASE_URL,
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-}
+// const optionsRequest = {
+//     uri: BASE_URL,
+//     transform: function (body) {
+//         return cheerio.load(body);
+//     }
+// }
 
-const error = (err) => {
-  throw new Error(err)
-}
-const success = ($) => {
-  let Dados = []
-  let obj = {}
-  // Aqui pegamos todos os objetos do DOM com essa classe '.tx_dados_herb'
-  $(ElementList).each(function(i, element){
-    // O VALOR correto vem em this.children[0].data 
-    // que está em Fields[i].value por isso o eval
-    if(options.conditionGetValues(i)) {
-      obj[Fields[i].name] = eval(Fields[i].value)
-    }
-    else if(options.conditionBreakList(i)) {
-      return callback(obj)
-    }
-  })
-}
+// const error = (err) => {
+//   throw new Error(err)
+// }
+// const success = ($) => {
+//   let Dados = []
+//   let obj = {}
+//   // Aqui pegamos todos os objetos do DOM com essa classe '.tx_dados_herb'
+//   $(ElementList).each(function(i, element){
+//     // O VALOR correto vem em this.children[0].data 
+//     // que está em Fields[i].value por isso o eval
+//     if(options.conditionGetValues(i)) {
+//       obj[Fields[i].name] = eval(Fields[i].value)
+//     }
+//     else if(options.conditionBreakList(i)) {
+//       return callback(obj)
+//     }
+//   })
+// }
 
-const options = {
-  conditionGetValues: (i) => i>0 && i<5,
-  conditionBreakList: (i) => i >= 5
-}
+// const options = {
+//   conditionGetValues: (i) => i>0 && i<5,
+//   conditionBreakList: (i) => i >= 5
+// }
 
-const callback = (obj) => { 
-  console.log('Dados: ', obj)
-  return false
-}
+// const callback = (obj) => { 
+//   console.log('Dados: ', obj)
+//   return false
+// }
 
 const Crawler = {
   BASE_URL: 'http://www.botanica.org.br/rbh-catalogo',
@@ -95,7 +95,7 @@ const Crawler = {
     }
   ],
   optionsRequest: {
-    uri: BASE_URL,
+    uri: 'http://www.botanica.org.br/rbh-catalogo',
     transform: function (body) {
         return cheerio.load(body);
     }
@@ -111,7 +111,7 @@ const Crawler = {
       if(Crawler.options.conditionGetValues(i)) {
         obj[Crawler.Fields[i].name] = eval(Crawler.Fields[i].value)
       }
-      else if(options.conditionBreakList(i)) {
+      else if(Crawler.options.conditionBreakList(i)) {
         return Crawler.callback(obj)
       }
     })
