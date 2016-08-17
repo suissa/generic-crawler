357ae93ffcf8b3e01bda1c7dc07f121ca825f40a
Suissa <jnascimento@gmail.com>
2016-08-17 19:43:03 -0300

    Exemplo com request e cheerio

--- src/index-request_cheerio.js
+++ src/index-request_cheerio.js
@@ -7,7 +7,7 @@ const crawlerData = require('./request-promise_cheerio/crawlerData')
 const crawlerConfig = require('./request-promise_cheerio/generateConfig')(crawlerData)
 // const crawlerGeneric = require('./request-promise_cheerio/genericCrawler')(crawlerConfig)
 
-const crawlerGeneric = require('./request_cheerio/genericCrawler')(crawlerData)
+const crawlerGeneric = require('./request_cheerio/genericCrawler')(crawlerConfig)
 
 
 // crawlerGeneric(crawlerConfig)
