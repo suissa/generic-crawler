2bfafc1a4ed0bc01b72392cacb5e6de92261f613
Suissa <jnascimento@gmail.com>
2016-12-08 20:35:15 -0200

    outro exemplo

---
+++ src/examples/ ingresse.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const SITE = 'ingresso.com'
+const CRAWLER = 'request-promise_cheerio'
+
+const crawlerData = require('./' + SITE + '/crawlerData')
+const crawlerConfig = require('./'+ CRAWLER +'/generateConfig')(crawlerData)
+const crawlerGeneric = require('./request-promise_cheerio/genericCrawler')(crawlerConfig)
+
+crawlerGeneric
+  .then(crawlerConfig.PROMISE_SUCCESS)
+  .catch(crawlerConfig.PROMISE_ERROR)
